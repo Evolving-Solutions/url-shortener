edfde2e55b93fe435f496793d74157844846313b
Riley Seaburg <riley@rileyseaburg.com>
2022-07-08 18:48:53 -0400

    Update Docker file to work with Rust.

--- src/routes/echo.rs
+++ src/routes/echo.rs
@@ -1,6 +1,6 @@
 use actix_web::{post, HttpResponse, Responder};
 
 #[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
+pub async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
