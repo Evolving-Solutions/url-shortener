d4107e1029ec7836a26a8853e1f4248822ee8a48
rileyseaburg <riley@rileyseaburg.com>
2022-07-18 03:18:17 -0400

    fix typo

--- rust.groovy
+++ rust.groovy
@@ -1,8 +1,7 @@
 job ('Build Project K API') {
     scm {
-        git('https://evolving-software@dev.azure.com/evolving-software/Business%20Management%20and%20Growth%20Platform/_git/backend_server_api') { node -> // is hudson.plugins.git.GitSCM
-            
-            node / gitConfigName('riley')
+        git('https://evolving-software@dev.azure.com/evolving-software/Business%20Management%20and%20Growth%20Platform/_git/backend_server_api') { node -> // is hudson.plugins.git.GitSCM    
+        node / gitConfigName('riley')
         node / gitConfigEmail('riley@evolvingsoftware.io')}
     }
     triggers {
@@ -11,7 +10,7 @@ job ('Build Project K API') {
     steps {
        dockerBuildAndPublish {
         repositoryName('evolvingsoftware/business-management-api')
-        tag('${GIT_REVISION,length=9')
+        tag('${GIT_REVISION,length=9}')
         registryCredentials('4e97f946-e36e-4299-b368-28dddb01539b')
         forcePull(false)
         forceTag(false)
