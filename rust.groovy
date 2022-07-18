6cd640cc36f0f740fc55bf677cc028d899234b1d
rileyseaburg <riley@rileyseaburg.com>
2022-07-18 02:38:56 -0400

    Update Groovy file

--- rust.groovy
+++ rust.groovy
@@ -1,13 +1,9 @@
-job ("Build Project K API") {
+job ('Build Project K API') {
     scm {
-        git('git@ssh.dev.azure.com:v3/evolving-software/Business%20Management%20and%20Growth%20Platform/backend_server_api') 
+        git('git@ssh.dev.azure.com:v3/evolving-software/Business%20Management%20and%20Growth%20Platform/backend_server_api')
     }
     triggers {
         scm('H/5 * * * *')
-
-    }
-    wrappers{
-        docker('docker')
     }
     steps {
        dockerBuildAndPublish {
