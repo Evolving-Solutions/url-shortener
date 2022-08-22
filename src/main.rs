71b6a5a825cb1ea03b8e31bb0ae3603da94cf4c0
Riley Seaburg <riley@rileyseaburg.com>
2022-08-22 19:06:54 -0400

    add ip logger

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use actix_web::{
     middleware::Logger, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result,
 };
 use env_logger;
+use local_ip_address::local_ip;
 use routes::url;
 use serde_json::json;
 use std::{env, path::PathBuf};
@@ -31,11 +32,17 @@ async fn index(req: HttpRequest) -> Result<NamedFile> {
 /// # Main web server
 /// Serves as the main entry point to the application.
 /// Publicly accessible.
+/// Log what port the server is running on.
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let local_ip = local_ip().unwrap();
+    let server_ip = local_ip.clone().to_string();
+    let port = ":8844".to_string();
+    let server_w_port = server_ip + &port;
     env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     env::set_var("RUST_BACKTRACE", "full");
+    println!("Listen on: {}", server_w_port);
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
@@ -45,7 +52,8 @@ async fn main() -> std::io::Result<()> {
             .service(url::delete_url)
         // .with_json_spec_at("/api/spec/v2")
     })
-    .bind("0.0.0.0:8844")?
+    .bind(server_w_port)?
     .run()
     .await
+
 }
