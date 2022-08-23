aa10f5e2ff0300614b2c343bd8d40e8c5d0891e4
Riley Seaburg <riley@rileyseaburg.com>
2022-08-23 13:52:43 -0400

    Fix connection error

--- src/db/connectdb.rs
+++ src/db/connectdb.rs
@@ -4,13 +4,12 @@ pub mod database {
 
     pub async fn connect_db() -> Client {
         let local_ip = local_ip().unwrap();
-        let server_ip = local_ip.clone().to_string();
-        let port = ":8844".to_string();
-        let mongo_uri = "mongodb://".to_string()
-            + &server_ip
-            + &port
-            + "/evolving_solutions?retryWrites=true&w=majority";
-        let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| mongo_uri.into());
+        let mongo_prefix = "mongodb://";
+        let mongo_prefix_and_ip = mongo_prefix.to_owned() + &local_ip.to_string();
+        let mongo_uri = mongo_prefix_and_ip.to_owned() + ":27017";
+        let mongo_connection_string =
+            mongo_uri.to_owned() + "/evolving_solutions?retryWrites=true&w=majority";
+        let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| mongo_connection_string.into());
         // create a mongo connection url that uses the local ip address
         let client = Client::with_uri_str(uri).await.expect("failed to connect");
         client
