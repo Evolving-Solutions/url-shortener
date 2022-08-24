b4644931e563a41e0a86750f536718eac81fb0e7
Riley Seaburg <riley@rileyseaburg.com>
2022-08-24 09:02:01 -0400

    Refactor db connection

--- src/db/connectdb.rs
+++ src/db/connectdb.rs
@@ -1,6 +1,6 @@
 pub mod database {
     use local_ip_address::local_ip;
-    use mongodb::Client;
+    use mongodb::{options::ClientOptions, Client};
 
     pub async fn connect_db() -> Client {
         let local_ip = local_ip().unwrap();
@@ -10,8 +10,25 @@ pub mod database {
         let mongo_connection_string =
             mongo_uri.to_owned() + "/evolving_solutions?directConnection=true&retryWrites=true";
         let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| mongo_connection_string.into());
-        // create a mongo connection url that uses the local ip address
-        let client = Client::with_uri_str(uri).await.expect("failed to connect");
-        client
+        let client_connection = uri.clone().to_string();
+        let client_options = ClientOptions::parse(client_connection).await;
+        match client_options {
+            Ok(options) => {
+                let mut client_options = options;
+                client_options.app_name = Some("url-shortener".to_string());
+                let client = Client::with_options(client_options);
+                match client {
+                    Ok(client) => client,
+                    Err(e) => {
+                        println!("Failed to connect: {}", e);
+                        panic!("Failed to connect: {}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                println!("{}", e);
+                panic!("Failed to connect.");
+            }
+        }
     }
 }
